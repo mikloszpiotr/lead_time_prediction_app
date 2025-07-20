412251790b7aabb8a44a01514efed77fe554d070
mikloszpiotr <mikloszpiotr@gmail.com>
2025-07-20 19:52:38 +0200

    Final version: dynamic model training + RMSE fix for Streamlit Cloud

--- utils/modeling.py
+++ utils/modeling.py
@@ -4,6 +4,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+import numpy as np
 
 def train_models(data):
     X = data.drop(columns=["lead_time"])
@@ -28,7 +29,7 @@ def train_models(data):
 def evaluate_model(model, X_test, y_test):
     preds = model.predict(X_test)
     mae = mean_absolute_error(y_test, preds)
-    rmse = mean_squared_error(y_test, preds, squared=False)
+    rmse = np.sqrt(mean_squared_error(y_test, preds))
     return mae, rmse
 
 def predict(model, input_scaled):
